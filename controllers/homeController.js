9d9cae9fe6358fa6a0b7b9b6d4e431611c05e4c4
Wyatt Barger <wyattbarger@icloud.com>
2023-09-21 20:44:56 -0500

    Added res.render to homepage route, wrapped res.render in try catch block.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,10 +4,15 @@ const {Stock} = require('../models'); // Import the Stock model using a deconstr
 router.get('/', async (req, res) => {
     try { // Added try-catch block to handle potential errors. 
         const stockPrices = await Stock.findAll({ // Declare stockPrices const to get stock data.
-            attributes: ['id', 'ticker', 'company', 'currentPrice', 'historicalPrices'], // Added the attributes as outlined in the Stock model.
+            attributes: ['id', 'ticker', 'company', 'currentPrice'], // Added the attributes as outlined in the Stock model.
             order: [['company', 'ASC']], // Order the results by the company name.
         });
-        res.json(stockPrices); // Sends the stock data as a JSON response.
+        try { // Added try catch to test for errors rendering the homepage.
+        res.render('homepage', {stockPrices}); // Added res.render to render the homepage template and pass it stockPrices.
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ message: 'There was an error rendering the homepage!' });
+        }
     } catch (err) { // Catch for the try-catch block.
         console.error(err); // Log for any errors that may occur.
         res.status(500).json({ message: 'Internal server error.'}) // Display a message to the user in the case of a bad 
