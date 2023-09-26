fe54568febe126665e8b0d3252050b4be8425029
Wyatt Barger <wyattbarger@icloud.com>
2023-09-25 20:31:08 -0500

    Added a console.log to homecontrollers to confirm the data for our stocks is passing correctly.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -7,16 +7,13 @@ router.get('/', async (req, res) => {
             attributes: ['id', 'ticker', 'company', 'currentPrice'], // Added the attributes as outlined in the Stock model.
             order: [['company', 'ASC']], // Order the results by the company name.
         });
-        try { // Added try catch to test for errors rendering the homepage.
-        res.render('homebody', { layout: 'main', stockPrices}); // Added res.render to render the main template and pass it stockPrices. Accomplished by pathing into layouts setting the view as main, using the same layout.
+        const stock = stockPrices.map((product) => product.get({ plain: true })) // Added .map to stockPrices so that the data will pass properly as an array.
+        res.render('homebody', { layout: 'main', stock}); // Added res.render to render the main template and pass it stockPrices. Accomplished by pathing into layouts setting the view as main, using the same layout.
+        console.log(stockPrices);
         } catch (err) {
             console.error(err);
             res.status(500).json({ message: 'There was an error rendering the homepage!' });
         }
-    } catch (err) { // Catch for the try-catch block.
-        console.error(err); // Log for any errors that may occur.
-        res.status(500).json({ message: 'Internal server error.'}) // Display a message to the user in the case of a bad 
-    }
 });
 
 module.exports = router; // Added module.exports allow the routes defined here to be used elsewhere in the application.
