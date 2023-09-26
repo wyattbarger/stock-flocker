75f81a02ffb4d76d9d2cadb18dcadb76e97ad9cb
ahmell <andrewmell10@gmail.com>
2023-09-25 21:22:00 -0500

    Code now populates homepage with seeded data

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,15 +4,14 @@ const {Stock} = require('../models'); // Import the Stock model using a deconstr
 router.get('/', async (req, res) => {
     try { // Added try-catch block to handle potential errors. 
         const stockPrices = await Stock.findAll({ // Declare stockPrices const to get stock data.
-            attributes: ['id', 'ticker', 'company', 'currentPrice'], // Added the attributes as outlined in the Stock model.
+            attributes: { include: ['id', 'ticker', 'company', 'currentPrice']}, // Added the attributes as outlined in the Stock model.
             order: [['company', 'ASC']], // Order the results by the company name.
         });
-        try { // Added try catch to test for errors rendering the homepage.
-        res.render('homebody', { layout: 'main', stockPrices}); // Added res.render to render the main template and pass it stockPrices. Accomplished by pathing into layouts setting the view as main, using the same layout.
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ message: 'There was an error rendering the homepage!' });
-        }
+
+         const stocks = stockPrices.map((stock) => stock.get({ plain: true }));
+         // Added try catch to test for errors rendering the homepage.
+        res.render('homebody', {stocks}); // Added res.render to render the main template and pass it stockPrices. Accomplished by pathing into layouts setting the view as main, using the same layout.
+        
     } catch (err) { // Catch for the try-catch block.
         console.error(err); // Log for any errors that may occur.
         res.status(500).json({ message: 'Internal server error.'}) // Display a message to the user in the case of a bad 
