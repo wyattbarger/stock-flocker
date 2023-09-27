335687062190a49405ed0a0f8e530ca40eadc57f
Wyatt Barger <wyattbarger@icloud.com>
2023-09-26 19:18:23 -0500

    Added the code necessary to render the login view when the appropriate api/users/login route is hit.

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -29,11 +29,11 @@ router.get('/users/:id', async (req, res) => {
 });
  
 router.get('/register', (req,res) => { // Added a GET route that will show the register page when the route /api/users/register is hit.
-  res.render('register', {layout: 'main' }); // Added code to properly target the register view while utilizing the main layout, created by jdgiancola. 
+  res.render('register', {layout: 'main'}); // Added code to properly target the register view while utilizing the main layout, created by jdgiancola. 
 });
 
 router.get('/login', async (req,res) => {
-  try
+    res.render('login', {layout: 'main'}); // Added code to render the login route with the main layout, created by jdgiancola.
 })
 
 router.post('/login', async (req,res) => { // Added a POST route for logging in.
