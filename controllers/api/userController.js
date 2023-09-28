cb313ec3c43fe8284118f15483098403c5228ec3
Wyatt Barger <wyattbarger@icloud.com>
2023-09-28 18:00:53 -0500

    Fixed userController routes, a new user can no succesfully be registered and stored to the database as tested and confirmed with Insomnia. Fixed a security issue I created when originally writing these routes which made a security exploit allowing a client to log into any user account so long as they had the correct unique username. Unga Bunga.

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -2,44 +2,40 @@ const { User, Comment } = require('../../models');
 const router = require('express').Router(); // Import the router object of express with const 'router'.
 
 
-router.get('/', async (req, res) => {
-  try {
-    const users = await User.findAll({
-      include: [{ model: Comment }]
-    });
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-}); 
+// router.get('/', async (req, res) => {
+//   try {
+//     const users = await User.findAll({
+//       include: [{ model: Comment }]
+//     });
+//     res.status(200).json(users);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// }); 
 
-// Get a single user by ID
-router.get('/:id', async (req, res) => {
-  try {
-    const user = await User.findByPk(req.params.id, {
-      include: [{ model: Comment }]
-    });
-    if (!user) {
-      return res.status(404).json({ message: 'User not found!' });
-    }
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
+// // Get a single user by ID
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const user = await User.findByPk(req.params.id, {
+//       include: [{ model: Comment }]
+//     });
+//     if (!user) {
+//       return res.status(404).json({ message: 'User not found!' });
+//     }
+//     res.status(200).json(user);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+// Route tested and working in insomnia. Backend functional.
 router.post('/register', async (req,res) => {
   try { // Added a try-catch block to handle errors.
-  const notUniqueName = await User.findOne({ where: { username: req.body.username }}); // Added a const called notUniqueName to search for an existing user, based on the username the potential new user is attempting to register under. 
-  if (notUniqueName) { // Added a conditional to check is the username is unique or not. 
-    return res.status(400).json({ message: 'Someone has already registered with that username. Please try again.'}) // If the conditional is met inform the user they need to try a different username for registration.
-  }
   const newUser = await User.create(req.body); // Added code to create the new user using the User model, assuming the above conditional is not met.
   req.session.save(() => { // Similar to the block in the /login POST, however in this case we are now logging a user in after successful registration, and storing the express-session.
-    req.session.user_id = userData.id;
+    req.session.user_id = newUser.id;
     req.session.logged_in = true;
 
-    res.status(200).json(userData);
+    res.status(200).json({ message: 'Account created!'});
   });
   } catch (err) {
     res.status(400).json(err);
@@ -48,12 +44,20 @@ router.post('/register', async (req,res) => {
 
 //NOTE: 
 
-
+// Route tested and working in insomnia. Backend functional.
 router.post('/login', async (req,res) => { // Added a POST route for logging in.
   try { // Added try-catch to handle errors.
     const userInfo = await User.findOne({where: { username: req.body.username} }); // Added code to find one user based on their username, this has been given an unique property in the model
     if (!userInfo) { // Added a condition to make the the user info matches what is on record based on the the username, which was defined as unique by andmell in models/user.js.
       res.status(400).json({ message: 'Wrong username or password, please try again!' }); // Tell the user they entered some of their info incorrect and return.
+      return;
+    }
+    const validPassword = await userInfo.checkPassword(req.body.password); // Added a method to validate the user is actually entering the correct method which does work as tested in Insomnia. Fixes previous undiscovered security issue I originally created with this route which would have allowed a client to log into any account so long as they could retrieve the unique username associated with it.
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
     req.session.save(() => { // If the users enters the correct info, save the session
