52a31579c7815afe2f8bf577bb03e2ad1b8fa84c
Wyatt Barger <wyattbarger@icloud.com>
2023-09-28 12:06:11 -0500

    Added a delete/comment/:id route that will be used to allow users to delete their own comments on a post. Needs to be tested in insomnia still (future commit will be made to confirm functionality. Edit still needs tested as well. /register route bug persists, and will be fixed in a feature/registerRouteBug branch.

--- controllers/api/commentController.js
+++ controllers/api/commentController.js
@@ -44,18 +44,35 @@ router.post('/post/:id', withAuth, async (req, res) => {
   }
 });
 
-router.put('/edit/comment/id:', withAuth, async (req,res) => { // Added put route for a authenticated user to update their choosen comment.
+router.put('/edit/comment/:id', withAuth, async (req,res) => { // Added put route for a authenticated user to update their choosen comment.
   try { // Added a try-catch block for handling errors.
     const commentId = req.params.id; // Declare commentId variable set to request the comment id. 
     const content = req.body.text; // Declare content variable set to the users updated text.
     const editedComment = await Comment.update(
-      {text},
+      {text}, // Update the data stored in the text column of the Comment model that has been selected based off of the current comment id.
       { where: { id: commentId }}
     );
-    res.status(200).json({ messaage: 'Your changes have been updated.' })
+    res.status(200).json({ message: 'Your changes have been updated.' }) // Log that the selected comment has been updated.
   } catch (err) {
-    res.status(500).json({ message: 'We were unable to process you edit request. Please try again later.'})
+    res.status(500).json({ message: 'We were unable to process you edit request. Please try again later.'}) // Log that the edit request was unseccessful.
   } 
+});
+
+router.delete('/delete/comment/:id', withAuth, async (req,res) => { // Added a route to delete a comment based on the comments id.
+  try { // Added a try-catch block for handling errors.
+    const comment = await Comment.destroy({ // Declare a variable using the destroy method to delete the choosen comment.
+      where: {
+        id: req.params.id, // Added parameters so that the deleted comment targeted is done so with req.params.id, an double checks that the user is deleting a comment that belongs to them, and not someone else's.
+        user_id: req.session.user_id,
+      },
+    });
+    if (!comment) { // Added a conditional so that the user is notified the comment they are trying to delete is unable to be found with the choosen id.
+      return res.status(404).json({ message: 'There was no comment found with that id. Confirm your selection and try again.' })
+    }
+    res.status(200).json(comment); // If the conditional isn't met then send a 200 response to the server.
+  } catch (err) {
+    res.status(500).json(err); // If there is an internal server issue throw an error.
+  }
 });
 
 // ... additional CRUD operations as necessary ...
