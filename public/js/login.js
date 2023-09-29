be47bbc51afb9a15e635685291c2d63488ec3a79
Wyatt Barger <wyattbarger@icloud.com>
2023-09-28 19:54:46 -0500

    Commiting current work on the front end login logic connection to merge pull request from Joseph.

---
+++ public/js/login.js
@@ -0,0 +1,17 @@
+const loginUser = async (event) => { // Created a loginUser async function to bridge the front end logic with the backend.
+    event.preventDefault();
+    const username = document.querySelector('#username').value.trim(); // Target the input element with the id username on line 8 of login.handlebars. value.trim() utilized to remove trailing or leading whitespace
+    const password = document.querySelector('#password').value.trim();
+    if (username && password) {
+        const serverResponse = await fetch('/api/users/login', { // Created a variable to call a fetch to the login homeroute with a POST method.
+            method: 'POST',
+            body: JSON.stringify({ username, password }), // Send the user input to the server as a JSON string.
+            headers: { 'Content-Type': 'application/json' }, // Indicates that the request body for the server is in JSON format.
+        });
+        if (serverResponse.ok) {
+            document.location.replace('/'); // Take the user back to the homepage if the serverResponse variable passes.
+        } else {
+            alert('There was a problem logging in. Please make sure you are uusing the correct credentials and try again.');
+        }
+    }
+};
