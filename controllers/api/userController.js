7654d3d7eb165789e2200256cad49577f674620e
ahmell <andrewmell10@gmail.com>
2023-09-30 15:13:03 -0500

    logout route functionality successfully added

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -1,6 +1,5 @@
-const { User, Comment } = require('../../models');
-const router = require('express').Router(); // Import the router object of express with const 'router'.
-
+const { User, Comment } = require("../../models");
+const router = require("express").Router(); // Import the router object of express with const 'router'.
 
 // router.get('/', async (req, res) => {
 //   try {
@@ -11,7 +10,7 @@ const router = require('express').Router(); // Import the router object of expre
 //   } catch (err) {
 //     res.status(500).json(err);
 //   }
-// }); 
+// });
 
 // // Get a single user by ID
 // router.get('/:id', async (req, res) => {
@@ -28,28 +27,37 @@ const router = require('express').Router(); // Import the router object of expre
 //   }
 // });
 // Route tested and working in insomnia. Backend functional.
-router.post('/register', async (req,res) => {
-  try { // Added a try-catch block to handle errors.
-  const newUser = await User.create(req.body); // Added code to create the new user using the User model, assuming the above conditional is not met.
-  req.session.save(() => { // Similar to the block in the /login POST, however in this case we are now logging a user in after successful registration, and storing the express-session.
-    req.session.user_id = newUser.id;
-    req.session.logged_in = true;
+router.post("/register", async (req, res) => {
+  try {
+    // Added a try-catch block to handle errors.
+    const newUser = await User.create(req.body); // Added code to create the new user using the User model, assuming the above conditional is not met.
+    req.session.save(() => {
+      // Similar to the block in the /login POST, however in this case we are now logging a user in after successful registration, and storing the express-session.
+      req.session.user_id = newUser.id;
+      req.session.logged_in = true;
 
-    res.status(200).json({ message: 'Account created!'});
-  });
+      res.status(200).json({ message: "Account created!" });
+    });
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-//NOTE: 
+//NOTE:
 
 // Route tested and working in insomnia. Backend functional.
-router.post('/login', async (req,res) => { // Added a POST route for logging in.
-  try { // Added try-catch to handle errors.
-    const userInfo = await User.findOne({where: { username: req.body.username} }); // Added code to find one user based on their username, this has been given an unique property in the model
-    if (!userInfo) { // Added a condition to make the the user info matches what is on record based on the the username, which was defined as unique by andmell in models/user.js.
-      res.status(400).json({ message: 'Wrong username or password, please try again!' }); // Tell the user they entered some of their info incorrect and return.
+router.post("/login", async (req, res) => {
+  // Added a POST route for logging in.
+  try {
+    // Added try-catch to handle errors.
+    const userInfo = await User.findOne({
+      where: { username: req.body.username },
+    }); // Added code to find one user based on their username, this has been given an unique property in the model
+    if (!userInfo) {
+      // Added a condition to make the the user info matches what is on record based on the the username, which was defined as unique by andmell in models/user.js.
+      res
+        .status(400)
+        .json({ message: "Wrong username or password, please try again!" }); // Tell the user they entered some of their info incorrect and return.
       return;
     }
     const validPassword = await userInfo.checkPassword(req.body.password); // Added a method to validate the user is actually entering the correct method which does work as tested in Insomnia. Fixes previous undiscovered security issue I originally created with this route which would have allowed a client to log into any account so long as they could retrieve the unique username associated with it.
@@ -57,37 +65,32 @@ router.post('/login', async (req,res) => { // Added a POST route for logging in.
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
-    req.session.save(() => { // If the users enters the correct info, save the session
+    req.session.save(() => {
+      // If the users enters the correct info, save the session
       req.session.user_id = userInfo.id; // Store the session based on the user id that logged in.
       req.session.logged_in = true; // Set the session logged_in property to true so that users can view pages requiring authentication.
       // res.status(200).json({ user: userInfo, message: 'You have successfully logged in. Welcome to Stock Flocker!' })
       if (req.session.logged_in) {
-        res.redirect('/');
+        res.redirect("/");
       }
     });
-  } catch (err) { 
+  } catch (err) {
     res.status(400).json(err);
   }
 });
 // ... additional CRUD operations as necessary ...
-
-router.get()
-
 
 router.post('/logout', (req, res) => {
   // When the user logs out, destroy the session
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
-      console.log(req.session);
     });
   } else {
     res.status(404).end();
-    console.log('error');
   }
 });
-
 module.exports = router;
