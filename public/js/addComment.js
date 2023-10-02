fd935c8220a8d9f32a221c7dce702007c3e3239e
ahmell <andrewmell10@gmail.com>
2023-10-02 00:00:32 -0500

    comment modal now closes when user clicks on X or cancel

---
+++ public/js/addComment.js
@@ -0,0 +1,16 @@
+const openModal = async (event) => {
+    event.preventDefault();
+    var commentModal = document.getElementById("commentModal");
+    commentModal.classList.add('is-active');
+};
+
+const closeModal = async (event) => {
+    event.preventDefault();
+    var commentModal = document.getElementById("commentModal");
+    commentModal.classList.remove('is-active');
+};
+
+
+document.querySelector('#add-comment').addEventListener('click', openModal);
+document.querySelector('#closeModal').addEventListener('click', closeModal);
+document.querySelector('#closeModal2').addEventListener('click', closeModal);
